240a5a8df692dea6f5f6a0c77b63d1f730eceb01
anurag8484 <anuragpoonia.ap@gmail.com>
2025-12-08 19:59:51 +0530

    feature2 completed

--- app/components/features2.tsx
+++ app/components/features2.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Features2 = () => {
@@ -7,6 +8,140 @@ const Features2 = () => {
         <h1 className="max-w-lg text-3xl font-semibold tracking-wide text-neutral-800">
           Run on dedicated, low-latency infra with no limits
         </h1>
+      </div>
+      <div className="mt-12 flex flex-col flex-wrap items-center justify-center gap-10">
+        <div className="flex w-full flex-col flex-wrap justify-center gap-10 rounded-3xl border-3 border-r-6 border-b-6 border-[#FFCD9F] bg-white px-12 py-12 sm:flex-nowrap">
+          <div className="flex flex-wrap items-center justify-center gap-10 lg:flex-nowrap">
+            <Image src={"/solana.avif"} alt="solana" height={200} width={200} />
+            <div>
+              <h1 className="max-w-lg text-2xl leading-relaxed font-semibold tracking-wide">
+                High-performance Solana RPC SaaS —{" "}
+                <span className="rounded-lg bg-[#FFCD9F] px-2 py-1">
+                  Coming Soon
+                </span>
+              </h1>
+              <p className="mt-3 text-xl leading-relaxed font-semibold tracking-widest">
+                Purpose-built Solana RPC with stable sub-100ms latency, fast TX
+                landing, and high-speed shred/data feeds. Designed for HFT,
+                searchers, and bots that need faster block awareness and
+                predictable execution.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-wrap items-center justify-center gap-10 lg:flex-nowrap">
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#FFCD9F] bg-[#FDF2EC] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/atom.svg"} alt="atom" height={50} width={50} />
+              <p className="text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#E3B4FA] bg-[#F8F3FF] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/prism.svg"} alt="atom" height={50} width={50} />
+              <p className="text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#B4DCFA] bg-[#ECF7FD] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/meter.svg"} alt="atom" height={50} width={50} />
+              <p className="max-w-lg text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+          </div>
+          <div>
+            <button className="w-fit cursor-pointer rounded-lg border-2 border-[#FF6928] bg-[#FF6928] px-4 py-4 text-xl font-semibold text-white transition duration-300 hover:border-transparent hover:bg-[#FF8540]">
+              Join Waitlist
+            </button>
+          </div>
+        </div>
+        <div className="flex w-full flex-col flex-wrap justify-center gap-10 rounded-3xl border-3 border-r-6 border-b-6 border-[#FFCD9F] bg-white px-12 py-12 sm:flex-nowrap">
+          <div className="flex flex-wrap items-center justify-center gap-10 lg:flex-nowrap">
+            <Image src={"/solana.avif"} alt="solana" height={200} width={200} />
+            <div>
+              <h1 className="max-w-lg text-2xl leading-relaxed font-semibold tracking-wide">
+                High-performance Solana RPC SaaS —{" "}
+                <span className="rounded-lg bg-[#FFCD9F] px-2 py-1">
+                  Coming Soon
+                </span>
+              </h1>
+              <p className="mt-3 text-xl leading-relaxed font-semibold tracking-widest">
+                Purpose-built Solana RPC with stable sub-100ms latency, fast TX
+                landing, and high-speed shred/data feeds. Designed for HFT,
+                searchers, and bots that need faster block awareness and
+                predictable execution.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-wrap items-center justify-center gap-10 lg:flex-nowrap">
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#FFCD9F] bg-[#FDF2EC] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/atom.svg"} alt="atom" height={50} width={50} />
+              <p className="text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#E3B4FA] bg-[#F8F3FF] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/prism.svg"} alt="atom" height={50} width={50} />
+              <p className="text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#B4DCFA] bg-[#ECF7FD] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/meter.svg"} alt="atom" height={50} width={50} />
+              <p className="max-w-lg text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+          </div>
+          <div>
+            <button className="w-fit cursor-pointer rounded-lg border-2 border-[#FF6928] bg-[#FF6928] px-4 py-4 text-xl font-semibold text-white transition duration-300 hover:border-transparent hover:bg-[#FF8540]">
+              Join Waitlist
+            </button>
+          </div>
+        </div>
+        <div className="flex w-full flex-col flex-wrap justify-center gap-10 rounded-3xl border-3 border-r-6 border-b-6 border-[#FFCD9F] bg-white px-12 py-12 sm:flex-nowrap">
+          <div className="flex flex-wrap items-center justify-center gap-10 lg:flex-nowrap">
+            <Image src={"/solana.avif"} alt="solana" height={200} width={200} />
+            <div>
+              <h1 className="max-w-lg text-2xl leading-relaxed font-semibold tracking-wide">
+                High-performance Solana RPC SaaS —{" "}
+                <span className="rounded-lg bg-[#FFCD9F] px-2 py-1">
+                  Coming Soon
+                </span>
+              </h1>
+              <p className="mt-3 text-xl leading-relaxed font-semibold tracking-widest">
+                Purpose-built Solana RPC with stable sub-100ms latency, fast TX
+                landing, and high-speed shred/data feeds. Designed for HFT,
+                searchers, and bots that need faster block awareness and
+                predictable execution.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-wrap items-center justify-center gap-10 lg:flex-nowrap">
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#FFCD9F] bg-[#FDF2EC] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/atom.svg"} alt="atom" height={50} width={50} />
+              <p className="text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#E3B4FA] bg-[#F8F3FF] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/prism.svg"} alt="atom" height={50} width={50} />
+              <p className="text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+            <div className="flex w-full flex-wrap items-center gap-3 rounded-3xl border-2 border-r-8 border-b-8 border-[#B4DCFA] bg-[#ECF7FD] px-4 py-4 lg:flex-nowrap">
+              <Image src={"/meter.svg"} alt="atom" height={50} width={50} />
+              <p className="max-w-lg text-lg leading-tight font-semibold text-neutral-700">
+                Smart workload routing over partners’ staked node network
+              </p>
+            </div>
+          </div>
+          <div>
+            <button className="w-fit cursor-pointer rounded-lg border-2 border-[#FF6928] bg-[#FF6928] px-4 py-4 text-xl font-semibold text-white transition duration-300 hover:border-transparent hover:bg-[#FF8540]">
+              Join Waitlist
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
