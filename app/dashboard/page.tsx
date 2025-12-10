0c07ba6c3b63d722e32790ac9111351182490806
anurag8484 <anuragpoonia.ap@gmail.com>
2025-12-10 12:02:30 +0530

    add dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,16 +4,17 @@ import React from "react";
 import ConnectWallet from "../components/connectWallet";
 import Navbar from "../components/navbar";
 import Container from "../components/container";
-import { Copy, ExternalLinkIcon } from "lucide-react";
+import { Copy, ExternalLinkIcon, RefreshCcw } from "lucide-react";
 import copy from "copy-to-clipboard";
 import { toast } from "sonner";
+import Link from "next/link";
 
 const page = () => {
   const connection = useConnection();
   const wallet = useWallet();
-  if (!wallet.connected) {
-    return <ConnectWallet />;
-  }
+  // if (!wallet.connected) {
+  //   return <ConnectWallet />;
+  // }
   return (
     <div className="h-screen w-full bg-[#FDF2EC]">
       <Container>
@@ -28,7 +29,7 @@ const page = () => {
             </p>
           </div>
           <div className="mt-10 flex items-center justify-center gap-10">
-            <div className="rounded-2xl border-2 border-[#FDBA74] bg-white px-8 py-8">
+            <div className="w-full rounded-2xl border-2 border-[#FDBA74] bg-white px-8 py-8">
               <div className="flex items-center justify-between">
                 <h3 className="rounded-xl bg-[#FED7AA] px-3 py-2 font-semibold text-[#7C2D12]">
                   Wallet Address
@@ -45,12 +46,41 @@ const page = () => {
               <p className="mt-5 rounded-xl bg-[#F9FAFB] px-4 py-4">
                 {wallet.publicKey?.toString() ?? "Not connected"}
               </p>
-              <div className="mt-4 flex cursor-pointer items-center gap-1 text-sm font-semibold text-[#FA7315]">
+              <Link
+                href={`https://explorer.solana.com/address/${wallet.publicKey}?cluster=devnet`}
+                target="blank"
+                className="mt-4 flex cursor-pointer items-center gap-1 text-sm font-semibold text-[#FA7315]"
+              >
                 <p>View in Explorer</p>
                 <ExternalLinkIcon size={17} />
-              </div>
+              </Link>
             </div>
-            <div>box2</div>
+            <div className="w-full rounded-2xl border-2 border-[#93C5FD] bg-white px-8 py-8">
+              <div className="flex items-center justify-between">
+                <h3 className="rounded-xl bg-[#C0DBFE] px-3 py-2 font-semibold text-[#1F3A8A]">
+                  Balance
+                </h3>
+                <RefreshCcw
+                  className="rounedd-2xl cursor-pointer px-2 py-2 transition duration-200 hover:bg-[#F9FAFB]"
+                  size={35}
+                  onClick={() => {
+                    copy(wallet.publicKey?.toString() ?? "no Wallet Key");
+                    toast("Public key copied to clipboard");
+                  }}
+                />
+              </div>
+              <p className="mt-5 text-3xl">
+                {wallet.publicKey?.toString() ?? "Not connected"}
+              </p>
+              <Link
+                href={`https://explorer.solana.com/address/${wallet.publicKey}?cluster=devnet`}
+                target="blank"
+                className="mt-4 flex cursor-pointer items-center gap-1 text-sm font-semibold text-[#FA7315]"
+              >
+                <p>View in Explorer</p>
+                <ExternalLinkIcon size={17} />
+              </Link>
+            </div>
           </div>
         </div>
       </Container>
